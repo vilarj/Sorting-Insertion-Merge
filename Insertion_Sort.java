477c9921e8211f41187a0b34cd6b2c2719e8ec3a
Vilarj <lamaravillasan@outlook.com>
2020-01-23 19:55:44 -0500

    Update Insertion_Sort.java

--- Insertion_Sort.java
+++ Insertion_Sort.java
@@ -1,10 +1,10 @@
 
-public class Insertion_Sort <T> {
-    public <T extends Comparable> Insertion_Sort (T[] list) {
+public class Insertion_Sort <T extends Comparable> {
+    public Insertion_Sort (T[] list) {
         Insertion (list);
     }
 
-    public <T extends Comparable> void Insertion (T[] list) {
+    public void Insertion (T[] list) {
         for (int i = 1; i < list.length; i++) {
             for (int j = i; j > 0; j--) {
                 if (list[j-1].compareTo(list[j]) > 0) {
@@ -15,13 +15,13 @@ public class Insertion_Sort <T> {
         }
     }
 
-    public <T extends Comparable> void swap(Comparable[] list, int i, int j) {
+    public void swap(Comparable[] list, int i, int j) {
         Comparable swap = list[i];
         list[i] = list[j];
         list[j] = swap;
     }
 
-    public <T extends  Comparable> T[] toArray (T[] list) {
+    public T[] toArray (T[] list) {
         for (T t : list) {System.out.print(t + " ");}
         return list;
     }
